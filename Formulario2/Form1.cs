f0441b8bb9ab19eee9ff5dd6e444964ee26ecfef
Ales017 <123915580+Ales017@users.noreply.github.com>
2023-02-24 00:30:53 -0600

    Formulario 2
    Examen I de lenguaje 3 2023

--- Formulario2/Form1.cs
+++ Formulario2/Form1.cs
@@ -51,43 +51,7 @@ namespace Formulario2
 
         private async void Calcularbutton_Click(object sender, EventArgs e)
         {
-            {
-                // Obtenemos los productos y precios del usuario
-                string[] productos = ProductotextBox.Text.Split(',');
-                decimal[] precios = Array.ConvertAll(PreciotextBox.Text.Split(','), decimal.Parse);
 
-                // Validamos que se hayan ingresado los mismos números de productos y precios
-                if (productos.Length != precios.Length)
-                {
-                    MessageBox.Show("Debe ingresar la misma cantidad de productos y precios.");
-                    return;
-                }
-
-                // Calculamos el total de la factura
-                decimal total = 0;
-                for (int i = 0; i < productos.Length; i++)
-                {
-                    total += precios[i];
-                }
-
-                // Calculamos el descuento asincrónicamente
-                decimal descuento = await CalcularDescuento(total);
-
-                // Mostramos los resultados al usuario
-                decimal totalConDescuento = total - descuento;
-                MessageBox.Show($"Descuento: ${descuento:0.00}");
-                MessageBox.Show($"Total con descuento: ${totalConDescuento:0.00}");
-            }
-
-            async Task<decimal> CalcularDescuento(decimal total)
-            {
-                // Simulamos un retraso de 2 segundos para simular un cálculo asincrónico
-                await Task.Delay(2000);
-
-                // Calculamos el descuento del 15% y lo devolvemos
-                decimal descuento = total * 0.15m;
-                return descuento;
-            }
         }
         
     }
