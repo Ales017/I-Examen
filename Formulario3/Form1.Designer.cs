a7c446932a45940a58fcd521fba6b71998118685
Ales017 <123915580+Ales017@users.noreply.github.com>
2023-02-25 12:03:12 -0600

    Formulario 3 Terminado
    Formulario 3 Terminado para lenguaje 3 IPeriodo 2023

---
+++ Formulario3/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Formulario3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MultiploslistBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MultiploslistBox
+            // 
+            this.MultiploslistBox.FormattingEnabled = true;
+            this.MultiploslistBox.ItemHeight = 16;
+            this.MultiploslistBox.Location = new System.Drawing.Point(57, 52);
+            this.MultiploslistBox.Name = "MultiploslistBox";
+            this.MultiploslistBox.Size = new System.Drawing.Size(622, 244);
+            this.MultiploslistBox.TabIndex = 0;
+            
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Mostrar Numeros";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MultiploslistBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox MultiploslistBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Formulario3/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> multiplos = new List<string>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    multiplos.Add("Darwin Sanchez");
+                }
+                else if (i % 3 == 0)
+                {
+                    multiplos.Add("Darwin");
+                }
+                else if (i % 5 == 0)
+                {
+                    multiplos.Add("Sanchez");
+                }
+                else
+                {
+                    multiplos.Add(i.ToString());
+                }
+            }
+
+            MultiploslistBox.DataSource = multiplos;
+        }
+    }
+}
